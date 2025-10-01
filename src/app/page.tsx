4ea627988722042fb4263ff90c16fb288c0e8109
luna industries <lunaindustrieske@gmail.com>
2025-10-01 10:20:37 +0000

    remove the Find Your Perfect Scent AI part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import ProductShowcase from '@/components/sections/product-showcase';
 import ParallaxSection from '@/components/sections/parallax-section';
 import SustainabilityHighlight from '@/components/sections/sustainability-highlight';
 import BlogSection from '@/components/sections/blog-section';
-import FragranceMoodGuide from '@/components/sections/fragrance-mood-guide';
 
 export default function Home() {
   return (
@@ -16,8 +15,6 @@ export default function Home() {
 
       <ParallaxSection imageId="parallax-kitchen" />
 
-      <FragranceMoodGuide />
-      
       <SustainabilityHighlight />
 
       <BlogSection />
