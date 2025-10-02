ed31a958175300cc87424ba3073450c18b8ff8c2
luna industries <lunaindustrieske@gmail.com>
2025-10-02 12:06:23 +0000

    kinda better but now remove products, DONT REMOVE ANY OTHER IMAGES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,8 @@ import ParallaxSection from '@/components/sections/parallax-section';
 import SustainabilityHighlight from '@/components/sections/sustainability-highlight';
 import BlogSection from '@/components/sections/blog-section';
 import AboutSection from '@/components/sections/about-section';
-import ProductShowcase from '@/components/sections/product-showcase';
-import { getAllProducts } from '@/lib/products';
 
 export default async function Home() {
-  const products = await getAllProducts();
   return (
     <>
       <HeroSection />
@@ -16,13 +13,9 @@ export default async function Home() {
 
       <ParallaxSection imageId="parallax-spa-bathroom" />
       
-      <ProductShowcase products={products.filter(p => p.category === 'Shower Gels')} />
-
       <SustainabilityHighlight />
       
       <ParallaxSection imageId="parallax-kitchen" />
-
-      <ProductShowcase products={products.filter(p => p.category === 'Dishwash' || p.category === 'Fabric Softeners')} />
 
       <BlogSection />
     </>
