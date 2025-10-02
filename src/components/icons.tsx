20cf235c5405a00ddd51f2b9670748c27fb2b6d7
luna industries <lunaindustrieske@gmail.com>
2025-10-02 10:56:50 +0000

    make the logo 2.3x bigger

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export function LunaLogo({ className, ...props }: SVGProps<SVGSVGElement> & {className?: string}) {
   return (
-    <div className={cn("relative w-20 h-10", className)}>
+    <div className={cn("relative w-48 h-24", className)}>
       <Image
         src="https://i.postimg.cc/2S5Kkf2m/logo-original.png"
         alt="Luna logo"
